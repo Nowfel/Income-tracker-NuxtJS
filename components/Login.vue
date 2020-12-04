3b580dd2b1178fbac4bdbb40f88c9b27a9824bfd
Nowfel <53569184+Nowfel@users.noreply.github.com>
2020-12-04 15:02:14 +0600

    loading added

--- components/Login.vue
+++ components/Login.vue
@@ -29,7 +29,17 @@
       label-size="sm"
       label-for="input-sm"
     >
-      <b-button type="submit" block variant="primary">Login</b-button>
+      <b-overlay
+        :show="busy"
+        rounded
+        opacity="0.6"
+        spinner-small
+        spinner-variant="primary"
+      >
+        <b-button block :disabled="busy" variant="primary" type="submit">
+          Login
+        </b-button>
+      </b-overlay>
     </b-form-group>
   </form>
 </template>
@@ -40,6 +50,7 @@ export default {
     return {
       email: "m@r.com",
       password: "123456",
+      busy: false,
     };
   },
   methods: {
@@ -48,14 +59,15 @@ export default {
         email: this.email,
         password: this.password,
       };
+      this.busy = true;
       this.$axios
         .post("login", object)
         .then((result) => {
           localStorage.setItem("token", result.data.token);
-          console.log('fsfsf',result.data.token);
+          this.$router.push("/income");
         })
-        .catch((err) => {});
-      console.log("submit");
+        .catch((err) => {})
+        .finally(() => (this.busy = false));
     },
   },
 };
