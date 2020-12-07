8c6e7f7e1280cd437c0c790a54fc1535b4f113ba
Nowfel <53569184+Nowfel@users.noreply.github.com>
2020-12-07 23:49:53 +0600

    dynamic route added

---
+++ pages/income/index/list/_id.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+dynamic income/list/{{nowfelId}}
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            nowfelId() {
+                return this.$route.params.id 
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
